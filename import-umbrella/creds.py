98644254e751425ebacc5ed74ac80e3c0535682e
areino <areino@users.noreply.github.com>
2022-05-05 13:05:32 +0200

    Update creds.py

--- import-umbrella/creds.py
+++ import-umbrella/creds.py
@@ -1,25 +1,19 @@
 
 ## XDR credentials (follow steps 1-5 at https://docs.ctpx.secureworks.com/apis/api_authenticate/)
 
-#xdrtenant   = 96072
-#xdrid       = "9EnbHC1ZB9ceRkhA6yXImXDE4tLPKTKS"
-#xdrsecret   = "6trWUHRkBtwVjqciqPil2LDFPMg98_Oi3xg49ZSsqZaKhZrQaiFKqJ4kn2i2zKSt"
 
-
-xdrtenant   = 135812
-xdrid       = "Q34G4ly3UYBHZw6FffmKHdmCBMYuOGQe"
-xdrsecret   = "WWPvYWgD6v7D6occb1bNaq2Ah2DDTEloltXn47noi3IJLdflkKiYrhlGdxFPS-gz"
+xdrtenant   = 
+xdrid       = ""
+xdrsecret   = ""
 
 
 
 
 ## S3 credentials (provided by Cisco Umbrella after setting up logging)
 
-s3datapath  = "cisco-managed-ap-southeast-2/5885386_b67ee652f566d4d76b5119432ddbfc4f7efecde6"
-
-
-s3accesskey = "AKIAWKWVZQY6JEMF3OEQ"
-s3secretkey = "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"
+s3datapath  = ""
+s3accesskey = ""
+s3secretkey = ""
 
 ## Other
 
